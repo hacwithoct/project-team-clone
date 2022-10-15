afc040bc3b5dfa44e45b369a20d007fa2c80ae3b
sachin-611 <shersingh6112000@gmail.com>
2022-10-15 20:14:12 +0530

    completed the server file code

--- index.js
+++ index.js
@@ -112,3 +112,67 @@ app.post("*", (req, res) => {
     errorMsg2: "What are you looking for?",
   });
 });
+// all the socket.io server side funtions
+io.on("connection", (socket) => {
+    // will save the message on the server when sended from chatroom
+    socket.on("sendMessage", async (msg, username, roomId) => {
+      const time = moment().format("LLL");
+      await addMessage(msg, username, time, roomId);
+      socket.emit("sendMessage", "done");
+    });
+    // sends all the messages to chatroom of the requested room
+    socket.on("getMessages", async (roomId) => {
+      socket.emit("getMessages", await getMessage(roomId));
+    });
+    // creates a new room and return its id
+    socket.on("createMeet", async () => {
+      let newroomid = uuidv4();
+      await createRoom(newroomid);
+      socket.emit("createMeet", newroomid);
+    });
+    // all the meeting funtions 
+    socket.on("join-room", (roomId, userId, userName) => {
+      // will save the new user record in database
+      userJoin(userId, userName, roomId);
+      // add user to room
+      socket.join(roomId);
+      // calls the function join room which will notify 
+      // all the other participants(in that meet) that new user has joined
+      socket.broadcast.to(roomId).emit("user-connected", userId);
+      // will save message to database
+      // will send request to all the users(in the meet) to add new message 
+      socket.on("message", async (message, id) => {
+        const sender = await getCurrentUser(id, roomId);
+        const time = moment().format("LLL");
+        addMessage(message, sender.username, time, roomId);
+        io.to(roomId).emit("addMessage", message, sender.username, time);
+      });
+      // called on disconnection od a user
+      // remove its record from server
+      // asks all the users to remove disconnected user
+      socket.on("disconnect", () => {
+        userLeave(userId, roomId);
+        io.to(roomId).emit("user-disconnected", userId);
+      });
+      // sends all the messages stored in the database of that meeting
+      socket.on("getAllMessage", async () => {
+        socket.emit("getAllMessage", await getMessage(roomId));
+      });
+      // removes the screen share form database
+      // asks users to remove from their grid
+      socket.on("remove", (sid) => {
+        userLeave(sid, roomId);
+        socket.to(roomId).emit("remove", sid);
+      });
+      // returns the list of all the participants
+      socket.on("getAllParticipants", async () => {
+        socket.emit("getAllParticipants", await getRoomUsers(roomId));
+      });
+      // return the name of the requested participant
+      socket.on("userNameReq", async (id) => {
+        socket.emit("userNameReq", await getUserName(id, roomId));
+      });
+    });
+  });
+  // listening to server
+  server.listen(port);
